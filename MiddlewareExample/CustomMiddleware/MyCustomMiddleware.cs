ce1cfde5b3e4ca07a8ebf46e686702959c9193d1
Jose Luis Contreras <joseluis.contreras@innocv.com>
2023-09-12 18:27:32 +0200

    First Commit

---
+++ MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace MiddlewareExample
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware - Starts\n");
+            await next(context);
+            await context.Response.WriteAsync("My Custom Middleware - Ends\n");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyCustomMiddleware>();
+        }
+    }
+}
+
---
+++ MiddlewareExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using MiddlewareExample;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
+var app = builder.Build();
+
+// middleware 1
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("From middleware 1\n");
+    await next(context); // <== calls middleware 2
+});
+
+// middleware 2
+//app.UseMiddleware<MyCustomMiddleware>();
+//app.UseMyCustomMiddleware();
+app.UseHelloCustomMiddleware();
+
+// middleware 3
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("From middleware 3\n");
+    // return to middleware 2
+});
+
+app.Run();
+
