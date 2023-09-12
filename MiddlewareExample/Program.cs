7e3ae963a8488ed632592ee6073e0000a2fc4b87
joseluiscontreras <joseluis.contreras@innocv.com>
2023-09-12 18:38:46 +0200

    UseWhenExample

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
+#region MiddlewareExample
+/*
 // middleware 1
 app.Use(async (context, next) =>
 {
@@ -22,6 +24,26 @@ app.Run(async (context) =>
     await context.Response.WriteAsync("From middleware 3\n");
     // return to middleware 2
 });
+*/
+#endregion
+
+#region UseWhenExample
+
+app.UseWhen(context => context.Request.Query.ContainsKey("username"),
+    app =>
+    {
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello from Middleware branch\n");
+            await next();
+        });
+    });
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello from middleware at main chain\n");
+});
+#endregion
 
 app.Run();
 
